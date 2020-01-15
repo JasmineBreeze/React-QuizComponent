977573dc4069c96cbd0a5de8c2bd3f167555b4d5
Jasmine Breeze <jasmine.breeze@bbc.co.uk>
2020-01-15 15:33:19 +0000

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,13 @@
 import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    render() {
+        return (
+           
+        );
+    }
+}
+
+export default Quiz;
