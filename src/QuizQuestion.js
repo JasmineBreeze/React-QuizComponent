958f991ca3b9b405c7beca2c42cdbf5393fc03a5
Jasmine Breeze <jasmine.breeze@bbc.co.uk>
2020-01-20 15:15:04 +0000

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -11,8 +11,8 @@ class QuizQuestion extends Component {
             </section>
             <section className="buttons">
               <ul>
-               <QuizQuestionButton />
-               {this.props.quiz_question.answer_options[0]}
+               <QuizQuestionButton button_text=
+               {this.props.quiz_question.answer_options[0]}/>
               </ul>
             </section>
           </main>
